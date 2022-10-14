0f214929fa103c6ac21d05174524b44ee5288c29
Milos Grabovcic <miksergrabo@gmail.com>
2022-10-14 12:36:19 +0200

    added code for printing out the final result

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 function getEmplFrom(path) {
 	return new Promise((resolve, reject) => {
 		var xhr = new XMLHttpRequest();
@@ -42,16 +41,19 @@ function abRmnSal(path) {
 async function getEmployees(order) {
 
 	//TODO should be exposed function which returns array of employees => for example [{id:"id", "name":"test", "salary":""}]
-	
+	//getting the employees and salaries
 	var emp = await getEmplFrom("./fake-server/employees.json")
 	var sal = await abRmnSal("./fake-server/salaries.json")
 
+	//combining the salaries and the employees into one array of objects
 	var arr3 = emp.map((item, i) => Object.assign({}, item, sal[i]));
 
+	//deleting the employeeId prop, since all i need is [{id:"id", "name":"test", "salary":""}]
 	arr3.forEach(object => {
 		delete object['employeeId'];
 	  });
 	
+	//returning the employees in ascending or descending order
 	if(order === "ascending"){
 		arr3.sort(function(a, b) {
 			return parseFloat(a.salary) - parseFloat(b.salary);
@@ -66,12 +68,14 @@ async function getEmployees(order) {
 		console.log("You need to choose between 'ascending' and 'descending'! ")
 	}
 	  
-	console.log(arr3)
+	
 	return arr3
 }
 
+//printing the result in the console
 var result = getEmployees("ascending");
-console.log(result);
-
+result.then(function(data) {
+	console.log(data) 
+ })
 
 module.exports = { getEmployees };
